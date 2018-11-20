34abcfc101b265345ae206991562ef6d1ff4ba92
Oscar David <44876247+OscarSTK98@users.noreply.github.com>
2018-11-19 22:05:18 -0600

    6

--- ProyectoWeb/web/Scripts/index.js
+++ ProyectoWeb/web/Scripts/index.js
@@ -26,3 +26,15 @@ new Vue({
   
 });
 /*----------------------------------------------------*/
+
+function Desarrolladores(){
+    
+    alert('Proyecto desarrollado por:\n\
+            \n\
+            Oscar David Ortega Prieto\n\
+            \n\
+            Daniel Adrian Gonzalez Nuñez\n\
+            \n\
+            Grupo 2CM7');
+    
+}

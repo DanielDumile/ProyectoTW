afebda112cd34f8388ba04a3baec09dcd75cd49e
DanielDumile <daniel_gn98@hotmail.com>
2018-11-19 23:29:59 -0600

    Login

---
+++ ProyectoWeb/src/java/Login.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+public class Login extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        
+        ServletContext context = request.getServletContext();
+        response.setContentType("text/html;charset=UTF-8");
+
+        String ruta=context.getRealPath("/")+"PruebaLogin.xml";
+        String usuario = request.getParameter("usuario");
+        String contrasena= request.getParameter("contrasena");        
+        
+        HttpSession sesion=request.getSession();
+        sesion.setAttribute("user",usuario);
+        sesion.setAttribute("rutaXML",ruta);
+        
+        
+        LoginBean lb = new LoginBean();
+        // Checamos si hay un usuario con dicho usuario y contrasena
+        String tipoUsuario = lb.validarUsuario(usuario,contrasena,ruta);
+        response.sendRedirect("Menu"+tipoUsuario);
+        /*
+        if(tipoUsuario != "ERROR"){
+            if(tipoUsuario.equals("Administrador")) response.sendRedirect("welcome");
+            else if(tipoUsuario.equals("Profesor")) response.sendRedirect("Profesor");
+            else if(tipoUsuario.equals("Alumno")) response.sendRedirect("Alumno");    
+        }
+        else{
+        	response.sendRedirect("Error");
+        }*/
+    }
+}
---
+++ ProyectoWeb/src/java/LoginBean.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import org.jdom.Element;
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+class LoginBean
+{
+    
+    String nombre="";
+    String pass="";
+    String tipoUsuario="";
+
+    /**
+     * Constructor for LoginBean
+     * Initializes the list of usernames/passwords
+     */
+    public LoginBean()
+    {
+        
+    }
+
+    /**
+     * determine if the username/password combination are
+     * present in the validUsers repository.
+     * param usuario
+     * param contrasena
+     * return String with user type
+     */
+    public String validarUsuario (String usuario, String contrasena, String ruta)
+    {
+        //HttpSession sesion=request.getSession();
+        SAXBuilder builder = new SAXBuilder();
+        File xmlFile = new File(ruta);
+        try{//Se crea el documento a traves del archivo  
+            Document document = (Document) builder.build( xmlFile );
+            //Se obtiene la raiz 
+            Element rootNode = document.getRootElement();
+            //Se obtiene la lista de hijos de la raiz 'usuarios'
+            List list = rootNode.getChildren( "user" );
+            for ( int i = 0; i < list.size(); i++ )
+            {
+               //Se obtiene el elemento 'user1'
+                Element campo = (Element) list.get(i);
+                //Se obtiene la lista de hijos del tag 'tabla'
+                nombre = campo.getChildTextTrim("nombre");
+                //Se obtiene el valor que esta entre los tags '&lt;valor&gt;&lt;/valor&gt;'
+                pass = campo.getChildTextTrim("password");
+                if(pass.equals(contrasena)){
+                    //Si la contrasena fue correcta, retorna el tipo de usuario.
+                    tipoUsuario= campo.getChildTextTrim("tipo");
+                    return tipoUsuario;    
+                }
+            }  
+        }catch ( JDOMException io ) {
+            System.out.println(io.getMessage());
+        }
+        return "ERROR";
+    }
+
+}

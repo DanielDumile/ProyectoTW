1829faec695f5e072151f2ea3973ff59cacdac2e
Oscar David <44876247+OscarSTK98@users.noreply.github.com>
2018-12-09 17:25:23 -0600

    VistaHotObject

--- ProyectoWeb/src/java/Administrador/MostrarPregunta.java
+++ ProyectoWeb/src/java/Administrador/MostrarPregunta.java
@@ -74,6 +74,7 @@ public class MostrarPregunta extends HttpServlet {
                         + "        \n"
                         + "    </head>\n"
                         + "    <body>");
+                
                 for (int i = 0; i < list.size(); i++) {
                     //Se obtiene el elemento 'user1'
                     Element campo = (Element) list.get(i);
@@ -81,7 +82,7 @@ public class MostrarPregunta extends HttpServlet {
                     String tipo = campo.getChildTextTrim("tipo");
                     String texto = campo.getChildTextTrim("texto");
                     String respuesta = campo.getChildTextTrim("respuesta");
-
+                    
                     //NUEVOS ELEMENTOS AQUI
                     String multimedia = campo.getChildTextTrim("multimedia");
                     String inicial = campo.getChildTextTrim("inicial");
@@ -94,62 +95,159 @@ public class MostrarPregunta extends HttpServlet {
                     //out.println(id);
                     System.out.println(id);
                     if (id_pregunta.equals(id)) {
-
+                        
                         if (tipo.equals("TrueFalse")) {
-                            out.println("");
-
-                            out.println("<div id=\"Preguntas\">\n"
-                                    + "            \n"
-                                    + "            <div id=\"Contenido\">\n"
-                                    + "                \n"
-                                    + "                <div id=\"Top\">\n"
-                                    + "                    \n"
-                                    + "                    <br />\n"
-                                    + "                    <Encabezado Pregunta=\"" + texto + "\" ></Encabezado> \n"
-                                    + "                    <input type=\"text\" value=\"" + id + "\" hidden />\n"
-                                    + "                </div>\n"
-                                    + "                \n"
-                                    + "                <br />\n"
-                                    + "                <br />\n"
-                                    + "                \n"
-                                    + "                <p class=\"Subtitulos3\" style=\"color: black\"><input type=\"radio\" value=\"Verdadero\" name=\"Valor\"/> Verdadero</p>\n"
-                                    + "                <p class=\"Subtitulos3\" style=\"color: black\"><input type=\"radio\" value=\"Falso\" name=\"Valor\"/> Falso</p>\n"
-                                    + "                 \n"
-                                    + "                <img class=\"Ad4\" src=\"" + multimedia + "\" alt=\"Examen\" />\n"
-                                    + "                \n"
-                                    + "                \n"
-                                    + "                 <div id=\"Bottom\" style=\"position: relative; left: 0px; top:190px;\" >\n"
-                                    + "                     <br />\n"
-                                    + "                     <p class=\"Subtitulos3\"><b>Intentos : </b><input type='text' value=" + intentos + " disabled name='try' class='Subtitulos3' id='try' width:30px; style='border: none; background-color:#4CAF50; color: white'/></p>\n"
-                                    + "                     <input type=\"button\" class=\"button2\"style=\"position: relative; left: 120px; top:-70px;\" onclick=\"Calificar2()\" id=\"Calificar\" value=\"Calificar\" />"
-                                    + "<input type=\"button\" class=\"button2\"style=\"position: relative; left: 120px; top:-70px;\" onclick=\"Regresar()\" id=\"Calificar\" value=\"Regresar\" />\n"
-                                    + "                </div>\n"
-                                    + "            </div>\n"
-                                    + "            \n");
-
-                            out.println("<input type='text' value='" + inicial + "' name='inicial' id='inicial' hidden />");
-                            out.println("<input type='text' value='" + evaluar + "' name='evaluar' id='evaluar' hidden />");
-                            out.println("<input type='text' value='" + correcta + "' name='correcta' id='correcta' hidden />");
-                            out.println("<input type='text' value='" + incorrecta + "' name='incorrecta' id='incorrecta' hidden />");
-                            out.println("<input type='text' value='" + intentar + "' name='intentar' id='intentar' hidden />");
+                           out.println("");
+                            
+                            
+                            out.println("<div id=\"Preguntas\">\n" +
+"            \n" +
+                                   
+"            <div id=\"Contenido\">\n" +
+"                \n" +
+"                <div id=\"Top\">\n" +
+"                    \n" +
+"                    <br />\n" +
+"                    <Encabezado Pregunta=\""+texto+"\" ></Encabezado> \n" +
+"                    <input type=\"text\" value=\""+id+"\" hidden />\n" +
+"                </div>\n" +
+"                \n" +
+"                <br />\n" +
+"                <br />\n" +
+"                \n" +
+"                <p class=\"Subtitulos3\" style=\"color: black\"><input type=\"radio\" value=\"Verdadero\" name=\"Valor\"/> Verdadero</p>\n" +
+"                <p class=\"Subtitulos3\" style=\"color: black\"><input type=\"radio\" value=\"Falso\" name=\"Valor\"/> Falso</p>\n" +
+"                 \n" +
+"                <img class=\"Ad4\" src=\""+multimedia+"\" alt=\"Examen\" />\n" +
+"                \n" +
+"                \n" +
+"                 <div id=\"Bottom\" style=\"position: relative; left: 0px; top:190px;\" >\n" +
+"                     <br />\n" +
+"                     <p class=\"Subtitulos3\"><b>Intentos : </b><input type='text' value="+intentos+" disabled name='try' class='Subtitulos3' id='try' width:30px; style='border: none; background-color:#4CAF50; color: white'/></p>\n" +
+"                     <input type=\"button\" class=\"button2\"style=\"position: relative; left: 120px; top:-70px;\" onclick=\"Calificar2()\" id=\"Calificar\" value=\"Calificar\" />"
+                                    + "<input type=\"button\" class=\"button2\"style=\"position: relative; left: 120px; top:-70px;\" onclick=\"Regresar()\" id=\"Calificar\" value=\"Regresar\" />\n" +
+"                </div>\n" +
+"            </div>\n" +
+"            \n");
+                            
+                            
+                            out.println("<input type='text' value='"+inicial+"' name='inicial' id='inicial' hidden />");
+                            out.println("<input type='text' value='"+evaluar+"' name='evaluar' id='evaluar' hidden />");
+                            out.println("<input type='text' value='"+correcta+"' name='correcta' id='correcta' hidden />");
+                            out.println("<input type='text' value='"+incorrecta+"' name='incorrecta' id='incorrecta' hidden />");
+                            out.println("<input type='text' value='"+intentar+"' name='intentar' id='intentar' hidden />");
                             out.println("<input type='text' value='No lo ocupo' name='answer' id='answer' hidden />");
-
-                            out.println("<input type='text' value='" + respuesta + "' name='Resp' id='Resp' hidden />"
-                                    + "            <div id=\"Left\" style=\"position: relative; left: 100px; top:-100px;\">\n"
-                                    + "                <p>Lea cuidadosamente la pregunta\n"
-                                    + "                y seleccione si es verdadera o falsa</p>\n"
-                                    + "                <p>Seleccione <b>Calificar</b> para obtener puntaje</p>\n"
-                                    + "                <p>Seleccione <b>Siguiente</b> para pasar a la otra pregunta</p>\n"
-                                    + "            </div>\n"
-                                    + "            \n"
-                                    + "            <div id=\"Left\" style=\"position: relative; left: -205px; top:130px;\">\n"
-                                    + "                <p><b>FeedBack</b></p>\n"
-                                    + "                <p><textarea id='feed' readonly style='width: 290px; height: 120px; background-color: white; text­align: center; border:none;'>" + inicial + "</textarea></p>\n"
-                                    + "            </div>\n"
-                                    + "        </div>");
-
+                           
+                            out.println("<input type='text' value='"+respuesta+"' name='Resp' id='Resp' hidden />"+
+                            
+"            <div id=\"Left\" style=\"position: relative; left: 100px; top:-100px;\">\n" +
+"                <p>Lea cuidadosamente la pregunta\n" +
+"                y seleccione si es verdadera o falsa</p>\n" +
+"                <p>Seleccione <b>Calificar</b> para obtener puntaje</p>\n" +
+"                <p>Seleccione <b>Siguiente</b> para pasar a la otra pregunta</p>\n" +
+"            </div>\n" +
+"            \n" +
+"            <div id=\"Left\" style=\"position: relative; left: -205px; top:130px;\">\n" +
+"                <p><b>FeedBack</b></p>\n" +
+"                <p><textarea id='feed' readonly style='width: 290px; height: 120px; background-color: white; text­align: center; border:none;'>"+inicial+"</textarea></p>\n" +
+"            </div>\n" +
+"        </div>");
+                            
                         } else {
-
+                            List lista = campo.getChildren("opcion");
+                            out.println("<input type='text' value='"+inicial+"' name='inicial' id='inicial' hidden />");
+                            out.println("<input type='text' value='"+evaluar+"' name='evaluar' id='evaluar' hidden />");
+                            out.println("<input type='text' value='"+correcta+"' name='correcta' id='correcta' hidden />");
+                            out.println("<input type='text' value='"+incorrecta+"' name='incorrecta' id='incorrecta' hidden />");
+                            out.println("<input type='text' value='"+intentar+"' name='intentar' id='intentar' hidden />");
+                            out.println("<input type='text' value='No lo ocupo' name='answer' id='answer' hidden />");
+                           
+                            out.println("<input type='text' value='"+respuesta+"' name='Resp' id='Resp' hidden />");
+                            
+                            out.println("<div id=\"Preguntas\">\n" +
+"            \n" +
+"            <div id=\"Contenido\" style=\"border-bottom: 6px solid #f0b60f;\">\n" +
+"                \n" +
+"                <div id=\"Top\" style=\"background-color:#f0b60f\">\n" +
+"                    \n" +
+"                    <br />\n" +
+"                    <Encabezado Pregunta=\""+texto+"\" ></Encabezado> \n" +
+"                    <input type=\"text\" value=\""+id+"\" hidden />\n" +
+"                </div>\n" +
+"                \n" +
+"                <br />\n" +
+"                <br />\n" +
+"                \n" +
+"                 <p>\n" +
+"                    <label class=\"checkeable\">\n" +
+"                        <input type=\"checkbox\" name=\"Respuesta\" value='1'/>\n" +
+"                        <img src=\""+((Element) lista.get(0)).getTextTrim()+"\" />\n" +
+"                    </label>\n" +
+"\n" +
+"                    <label class=\"checkeable\">\n" +
+"                      <input type=\"checkbox\" name=\"Respuesta\" value='2'/>\n" +
+"                      <img src=\""+((Element) lista.get(1)).getTextTrim()+"\" />\n" +
+"                    </label>\n" +
+"\n" +
+"                    <label class=\"checkeable\">\n" +
+"                        <input type=\"checkbox\" name=\"Respuesta\" value='3'/>\n" +
+"                        <img src=\""+((Element) lista.get(2)).getTextTrim()+"\" />\n" +
+"                    </label>\n" +
+"\n" +
+"                    <label class=\"checkeable\">\n" +
+"                      <input type=\"checkbox\" name=\"Respuesta\" value='4'/>\n" +
+"                      <img src=\""+((Element) lista.get(3)).getTextTrim()+"\" />\n" +
+"                    </label>\n" +
+"                </p>\n" +
+"                \n" +
+"                <p>\n" +
+"                    <label class=\"checkeable\">\n" +
+"                        <input type=\"checkbox\" name=\"Respuesta\" value='5'/>\n" +
+"                        <img src=\""+((Element) lista.get(4)).getTextTrim()+"\" />\n" +
+"                    </label>\n" +
+"\n" +
+"                    <label class=\"checkeable\">\n" +
+"                      <input type=\"checkbox\" name=\"Respuesta\" value='6'/>\n" +
+"                      <img src=\""+((Element) lista.get(5)).getTextTrim()+"\" />\n" +
+"                    </label>\n" +
+"                    <label class=\"checkeable\">\n" +
+"                        <input type=\"checkbox\" name=\"Respuesta\" value='7'/>\n" +
+"                        <img src=\""+((Element) lista.get(6)).getTextTrim()+"\" />\n" +
+"                    </label>\n" +
+"\n" +
+"                    <label class=\"checkeable\">\n" +
+"                      <input type=\"checkbox\" name=\"Respuesta\" value='8'/>\n" +
+"                      <img src=\""+((Element) lista.get(6)).getTextTrim()+"\" />\n" +
+"                    </label>\n" +
+"                    \n" +
+"                    <img class=\"Ad6\" src=\""+multimedia+"\" alt=\"Examen\" />\n" +
+"                </p>\n" +
+"                \n" +
+"                \n" +
+"                \n" +
+"                \n" +
+"                 <div id=\"Bottom\" style=\"position: relative; left: 0px; top:20px; background-color:#f0b60f\" >\n" +
+"                     <br />\n" +
+"                     <p class=\"Subtitulos3\"><b>Intentos : </b><input type='text' value="+intentos+" disabled name='try' class='Subtitulos3' id='try' width:30px; style='border: none; background-color:#4CAF50; color: white'/></p>\n" +
+"                     <input type=\"button\" class=\"button2\"style=\"position: relative; left: 120px; top:-70px;\" onclick=\"Calificar3()\" id=\"Calificar\" value=\"Calificar\" />"
+                                    + "<input type=\"button\" class=\"button2\"style=\"position: relative; left: 120px; top:-70px;\" onclick=\"Regresar()\" id=\"Calificar\" value=\"Regresar\" />\n" +
+"               </div>\n" +
+"            </div>\n" +
+"            \n" +
+"            <div id=\"Left\" style=\"position: relative; left: 100px; top: -100px; border: 4px solid #f0b60f;\">\n" +
+"                <p>Lea cuidadosamente la pregunta\n" +
+"                y seleccione si es verdadera o falsa</p>\n" +
+"                <p>Seleccione <b>Calificar</b> para obtener puntaje</p>\n" +
+"                <p>Seleccione <b>Siguiente</b> para pasar a la otra pregunta</p>\n" +
+"            </div>\n" +
+"            \n" +
+"            <div id=\"Left\" style=\"position: relative; left: -205px; top:130px; border: 4px solid #f0b60f;\">\n" +
+"                <p><b>FeedBack</b></p>\n" +
+"                <p><textarea id='feed' readonly style='width: 290px; height: 120px; background-color: white; text­align: center; border:none;'>"+inicial+"</textarea></p>\n" +
+"            </div>\n" +
+"        </div>");                         
+                            
+                            
                         }
                         break;
                     }
--- ProyectoWeb/src/java/Examen/NewExamen.java
+++ ProyectoWeb/src/java/Examen/NewExamen.java
@@ -118,7 +118,7 @@ public class NewExamen extends HttpServlet {
                 out.println("</div>");
                 out.println("</div>"
                         + "<input type=\"submit\" class=\"button2\" value=\"Subir Examen\" style=\"position: relative; top: 60px; left: -25px;\" />\n"
-                        + "                <input type=\"button\" class=\"button3\" value=\"Regresar\" onclick=\"RegresarExamen()\" style=\"position: relative; top: 60px; left: 10px;\" />"
+                        + "<input type=\"button\" class=\"button3\" value=\"Regresar\" onclick=\"RegresarExamen()\" style=\"position: relative; top: 60px; left: 10px;\" />"
                         + "</form>"
                         + "</div>"
                         + "<script src=\"../Scripts/index.js\"></script>");

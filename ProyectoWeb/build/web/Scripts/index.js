da885dd012b9a9912966a7e6706b51eb97c8920e
DanielDumile <daniel_gn98@hotmail.com>
2018-12-09 10:24:40 -0600

    examen y script

--- ProyectoWeb/build/web/Scripts/index.js
+++ ProyectoWeb/build/web/Scripts/index.js
@@ -1,69 +1,64 @@
 Vue.component('Encabezado', {
-  // declara las propiedades
-  props: ['Pregunta'],
-  // como 'data', las propiedades pueden ser utilizadas dentro de las
-  // plantillas y está disponibles en la vm como this.message
-  template: '<p class="Subtitulos3"> {{Pregunta}} </p>'
-        
+    // declara las propiedades
+    props: ['Pregunta'],
+    // como 'data', las propiedades pueden ser utilizadas dentro de las
+    // plantillas y está disponibles en la vm como this.message
+    template: '<p class="Subtitulos3"> {{Pregunta}} </p>'
+
 });
 
 new Vue({
- el: '#Top' 
+    el: '#Top'
 });
 
 
 
 new Vue({
-  el: '#Botones',
-
-  data: {
-    show: false,
-    show2: false,
-    show3: false,
-    show4: false,
-    show5: false
-  },
-  
+    el: '#Botones',
+    data: {
+        show: false,
+        show2: false,
+        show3: false,
+        show4: false,
+        show5: false
+    },
 });
 
 
 
 /* eslint-disable no-new */
 var vm = new Vue({
-  el: document.body,
-  data: {
-    a: 0,
-    b: 1
-  }
+    el: document.body,
+    data: {
+        a: 0,
+        b: 1
+    }
 });
 
 /*----------------------------------------------------*/
 //Login
 new Vue({
-  el: "#Login",
+    el: "#Login",
+    data: {
+        user: "",
+        pass: "",
+    },
+    methods: {
+        sub: function (event) {
 
-  data:{
-      user: "",
-      pass: "",
-  },
-  
-  methods: {
-      
-      sub: function(event){
-          
-          if(this.user == "" || this.pass == ""){
-            
-            alert('Completa los campos');
-            event.preventDefault();
-            
-          } 
-      }
-  }
-  
+            if (this.user == "" || this.pass == "") {
+
+                alert('Completa los campos');
+                event.preventDefault();
+
+            }
+        }
+    }
+
 });
 
 
- 
+
 /*----------------------------------------------------*/
 //Plantilla preguntas True False
 
@@ -71,8 +66,8 @@ new Vue({
 
 /*----------------------------------------------------*/
 
-function Desarrolladores(){
-    
+function Desarrolladores() {
+
     alert('Proyecto desarrollado por:\n\
             \n\
             Oscar David Ortega Prieto\n\
@@ -80,292 +75,292 @@ function Desarrolladores(){
             Daniel Adrian Gonzalez Nuñez\n\
             \n\
             Grupo 2CM7');
-    
-}
-
-function Cerrar(){
-    
-   location.href ="/ProyectoWeb/Vistas/Opciones.html"; 
 
 }
 
-function TrueorFalse(){
-    
-   location.href ="/ProyectoWeb/Vistas/TrueFalse.html"; 
-   
-    
-}
+function Cerrar() {
 
-function Regresar(){
-    
-   location.href ="/ProyectoWeb/Vistas/Administrador.html"; 
+    location.href = "/ProyectoWeb/Vistas/Opciones.html";
 
 }
 
-function RegresarExamen(){
-    
-    location.href ="/ProyectoWeb/Vistas/Examen.html"; 
-    
+function TrueorFalse() {
+
+    location.href = "/ProyectoWeb/Vistas/TrueFalse.html";
+
+
 }
 
-function NewExamen(){
-    
-    location.href ="/ProyectoWeb/NewExamen"; 
-    
+function Regresar() {
+
+    location.href = "/ProyectoWeb/Vistas/Administrador.html";
+
 }
 
-function HotObject(){
-    
-   location.href ="/ProyectoWeb/Vistas/HotObject.html"; 
-   
-    
+function RegresarExamen() {
+
+    location.href = "/ProyectoWeb/Vistas/Examen.html";
+
 }
 
-function Modificar(){
-    
-   location.href ="/ProyectoWeb/Modificar"; 
-   
-    
+function NewExamen() {
+
+    location.href = "/ProyectoWeb/NewExamen";
+
 }
 
-function ModificarExamen(){
-    
-    location.href ="/ProyectoWeb/ModE"; 
-    
+function HotObject() {
+
+    location.href = "/ProyectoWeb/Vistas/HotObject.html";
+
+
 }
 
-function Eliminar(){
-    
-   location.href = "/ProyectoWeb/Eliminar"; 
-   
-    
+function Modificar() {
+
+    location.href = "/ProyectoWeb/Modificar";
+
+
 }
 
-function RealizarExamen(){
-    
-    location.href = "/ProyectoWeb/MostrarResolver"; 
+function ModificarExamen() {
+
+    location.href = "/ProyectoWeb/ModE";
+
 }
 
-function VerExamen(){
-    
-    location.href = "/ProyectoWeb/MostrarExamenes"; 
+function Eliminar() {
+
+    location.href = "/ProyectoWeb/Eliminar";
+
+
 }
 
-function EliminarExamen(){
-    
-    location.href = "/ProyectoWeb/BorrarExamen"; 
-    
-    
+function RealizarExamen() {
+
+    location.href = "/ProyectoWeb/MostrarResolver";
 }
 
-function Ver(){
-    
-   location.href = "/ProyectoWeb/Ver"; 
-   
-    
+function VerExamen() {
+
+    location.href = "/ProyectoWeb/MostrarExamenes";
 }
 
-function Poner(id,i,total){
-    
+function EliminarExamen() {
+
+    location.href = "/ProyectoWeb/BorrarExamen";
+
+
+}
+
+function Ver() {
+
+    location.href = "/ProyectoWeb/Ver";
+
+
+}
+
+function Poner(id, i, total) {
+
     var Value;
-    for(var j = 0; j < total; j++){
+    for (var j = 0; j < total; j++) {
         Value = document.getElementById(j);
         document.getElementById(j).style.backgroundColor = '#a93226';
         document.getElementById(j).style.color = 'white';
-        Value.value='Seleccionar';
-        
-        
+        Value.value = 'Seleccionar';
+
+
     }
     Value = document.getElementById(i);
     document.getElementById(i).style.backgroundColor = 'white';
     document.getElementById(i).style.color = 'black';
-    Value.value='Seleccionada';
-    
+    Value.value = 'Seleccionada';
+
     var Sel = document.getElementById('ID');
-  
-    Sel.value=id;
-    
 
-    
+    Sel.value = id;
+
+
+
 }
 
-function EliminarPregunta(){
-    
+function EliminarPregunta() {
+
     var id = document.getElementById("ID").value;
-    if(id ===" "){
-        
+    if (id === " ") {
+
         alert('No selecciono ninguna pregunta');
-    }else{
-        
-         if(confirm("Esta seguro de eliminar la pregunta")){
-            document.getElementById("f1").submit(); 
+    } else {
+
+        if (confirm("Esta seguro de eliminar la pregunta")) {
+            document.getElementById("f1").submit();
         }
-       
+
     }
-    
-   
+
+
 }
 
 
-function ModificarPregunta(){
-     var id = document.getElementById("ID").value;
-   if(id === " "){
-        
+function ModificarPregunta() {
+    var id = document.getElementById("ID").value;
+    if (id === " ") {
+
         alert('No selecciono ninguna pregunta');
-    }else{
-    document.getElementById("f1").submit(); 
+    } else {
+        document.getElementById("f1").submit();
     }
 }
 
-function VerPregunta(){
-     var id = document.getElementById("ID").value;
-   if(id === " "){
-        
+function VerPregunta() {
+    var id = document.getElementById("ID").value;
+    if (id === " ") {
+
         alert('No selecciono ninguna pregunta');
-    }else{
-    document.getElementById("f1").submit(); 
+    } else {
+        document.getElementById("f1").submit();
     }
 }
 
-function Index(){
-    
-     location.href = "/ProyectoWeb/index.html"; 
-    
+function Index() {
+
+    location.href = "/ProyectoWeb/index.html";
+
 }
 
-function Examen(){
-    
-     location.href = "/ProyectoWeb/Vistas/Examen.html"; 
-    
+function Examen() {
+
+    location.href = "/ProyectoWeb/Vistas/Examen.html";
+
 }
 
-function Calificar(){
+function Calificar() {
 
-            var resultado;
- 
-            var porNombre=document.getElementsByName("Valor");
-            var Respuesta = document.getElementById("Resp").value;
+    var resultado;
 
-            for(var i=0;i<porNombre.length;i++) {
-                
-                if(porNombre[i].checked){
-                    resultado=porNombre[i].value;
-                    
-                    
-                }
-            }
-            var ans= document.getElementById("answer");
-            if(resultado === Respuesta){
-                ans.value='Correcta';
-                alert('Respuesta Correcta');
-                
-            }else{
-                ans.value='Incorrecta';
-                alert('Respuesta Incorrecta');
-                
-            }
-    
-    
+    var porNombre = document.getElementsByName("Valor");
+    var Respuesta = document.getElementById("Resp").value;
+
+    for (var i = 0; i < porNombre.length; i++) {
+
+        if (porNombre[i].checked) {
+            resultado = porNombre[i].value;
+
+
+        }
+    }
+    var ans = document.getElementById("answer");
+    if (resultado === Respuesta) {
+        ans.value = 'Correcta';
+        alert('Respuesta Correcta');
+
+    } else {
+        ans.value = 'Incorrecta';
+        alert('Respuesta Incorrecta');
+
+    }
+
+
 }
 
-function Siguiente(){
-    
+function Siguiente() {
+
     var index = document.getElementById("indice");
     var aux = parseInt(document.getElementById("indice").value);
     //aux = aux + 1; 
     index.value = aux;
-    
+
     var dir = document.getElementById("direccion");
-    dir.value='Siguiente';
-    
+    dir.value = 'Siguiente';
+
     document.getElementById("formi").submit();
-    
-    
-    
+
+
+
 }
 
-function Evaluacion(){
-    
+function Evaluacion() {
+
     var index = document.getElementById("indice");
     var aux = parseInt(document.getElementById("indice").value);
     //aux = aux + 1; 
     index.value = aux;
-    
+
     var dir = document.getElementById("direccion");
-    dir.value='Evaluacion';
-    
+    dir.value = 'Evaluacion';
+
     document.getElementById("formi").submit();
-    
-    
-    
+
+
+
 }
 
-function Anterior(){
-    
+function Anterior() {
+
     var index = document.getElementById("indice");
     var aux = parseInt(document.getElementById("indice").value);
     //aux = aux - 1;
     index.value = aux;
-    
+
     var dir = document.getElementById("direccion");
-    dir.value='Anterior';
-    
+    dir.value = 'Anterior';
+
     document.getElementById("formi").submit();
-    
-    
-    
+
+
+
 }
 
-function CalificarHotObject(){
-    
-    var Marcadas = document.getElementsByName("P");
-            var Correcta = document.getElementById("Correcta").value;
-            var Array = Correcta.split(",");
+function CalificarHotObject() {
 
-            var Contador = 0;
-            var Check = 0;
-            
-            for(var i = 0; i < Marcadas.length; i++){
-                
-                for(var j = 0; j < Array.length; j++){
-                    
-                    if(Marcadas[i].checked){
-                        Check++;
-  
-                        if(Marcadas[i].value === Array[j]){
-                            
-                            Contador++;
-                    
-                        }
-                        
-                    }else{
-                        
-                        break;
-                    }
+    var Marcadas = document.getElementsByName("Respuesta");
+    var Correcta = document.getElementById("Resp").value;
+    var Array = Correcta.split(",");
+
+    var Contador = 0;
+    var Check = 0;
+
+    for (var i = 0; i < Marcadas.length; i++) {
+
+        for (var j = 0; j < Array.length; j++) {
+
+            if (Marcadas[i].checked) {
+                Check++;
+
+                if (Marcadas[i].value === Array[j]) {
+
+                    Contador++;
+
                 }
-                
+
+            } else {
+
+                break;
             }
-            var Division = Check/Array.length;
-            
-            if(Contador === Array.length){
-                
-                if(Division === Contador){
-                    document.getElementById("answer").value='Correcta';
-                     alert('Respuesta Correcta');
-                }else{
-                    document.getElementById("answer").value='Incorrecta';
-                    alert('Respuesta Incorrecta');
-                    
-                }
-                
-            }else{
-                document.getElementById("answer").value='Incorrecta';
-                 alert('Respuesta Incorrecta');
-                
-            }
-    
+        }
+
+    }
+    var Division = Check / Array.length;
+
+    if (Contador === Array.length) {
+
+        if (Division === Contador) {
+            document.getElementById("answer").value = 'Correcta';
+            alert('Respuesta Correcta');
+        } else {
+            document.getElementById("answer").value = 'Incorrecta';
+            alert('Respuesta Incorrecta');
+
+        }
+
+    } else {
+        document.getElementById("answer").value = 'Incorrecta';
+        alert('Respuesta Incorrecta');
+
+    }
+
 }
 
 
-        
+
 

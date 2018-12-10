8ef0a00bb49575985f1dbdd7dcf01e05bde358f0
DanielDumile <daniel_gn98@hotmail.com>
2018-12-09 18:46:54 -0600

    ok

--- ProyectoWeb/src/java/Examen/ResolverExamen.java
+++ ProyectoWeb/src/java/Examen/ResolverExamen.java
@@ -104,9 +104,7 @@ public class ResolverExamen extends HttpServlet {
                 out.println("<input type='text' name='indice' value='" + String.valueOf(indice) + "' hidden>");
                 out.println("</form></body>");
                 out.println("</html>");
-                
-                
-                
+
             } else {
                 //SACAMOS A TODAS LAS PREGUNTAS PERO SOLO PONEMOS A LA QUE NOS INTERESA
                 SAXBuilder builder2 = new SAXBuilder();
@@ -182,13 +180,13 @@ public class ResolverExamen extends HttpServlet {
                                         + "                </div>\n"
                                         + "            </div>\n"
                                         + "            \n");
-                                
+
                                 out.println("<input type='text' value='" + inicial + "' name='inicial' id='inicial' hidden />");
                                 out.println("<input type='text' value='" + evaluar + "' name='evaluar' id='evaluar' hidden />");
                                 out.println("<input type='text' value='" + correcta + "' name='correcta' id='correcta' hidden />");
                                 out.println("<input type='text' value='" + incorrecta + "' name='incorrecta' id='incorrecta' hidden />");
                                 out.println("<input type='text' value='" + intentar + "' name='intentar' id='intentar' hidden />");
-                                
+
                                 out.println("<input type='text' value='" + respuesta + "' name='Resp' id='Resp' hidden />"
                                         + "            <div id=\"Left\" style=\"position: relative; left: 100px; top:-100px;\">\n"
                                         + "                <p>Lea cuidadosamente la pregunta\n"
@@ -199,7 +197,7 @@ public class ResolverExamen extends HttpServlet {
                                         + "            \n"
                                         + "            <div id=\"Left\" style=\"position: relative; left: -205px; top:130px;\">\n"
                                         + "                <p><b>FeedBack</b></p>\n"
-                                        + "                <p><input type='text' id='feed' value='"+inicial+"' readonly ></p>\n"
+                                        + "                <p><input type='text' id='feed' value='" + inicial + "' readonly ></p>\n"
                                         + "            </div>\n"
                                         + "        </div>");
 
@@ -244,6 +242,100 @@ public class ResolverExamen extends HttpServlet {
                                         + "        </div>");*/
                             } else {
                                 List lista = campo.getChildren("opcion");
+                                out.println("<input type='text' value='" + inicial + "' name='inicial' id='inicial' hidden />");
+                                out.println("<input type='text' value='" + evaluar + "' name='evaluar' id='evaluar' hidden />");
+                                out.println("<input type='text' value='" + correcta + "' name='correcta' id='correcta' hidden />");
+                                out.println("<input type='text' value='" + incorrecta + "' name='incorrecta' id='incorrecta' hidden />");
+                                out.println("<input type='text' value='" + intentar + "' name='intentar' id='intentar' hidden />");
+                                out.println("<div id=\"Preguntas\">\n"
+                                        + "            \n"
+                                        + "            <div id=\"Contenido\" style=\"border-bottom: 6px solid #f0b60f;\">\n"
+                                        + "                \n"
+                                        + "                <div id=\"Top\" style=\"background-color:#f0b60f\">\n"
+                                        + "                    \n"
+                                        + "                    <br />\n"
+                                        + "                    <Encabezado Pregunta=\"" + texto + "\" ></Encabezado> \n"
+                                        + "                    <input type=\"text\" value=\"" + idExamen + "\" name='ID' hidden />\n"
+                                        + "     <input type='text' id='indice' name='indice' value='" + String.valueOf(indice) + "' hidden>"
+                                        + "     <input type='text' id='cantidad' name='cantidad' value='" + String.valueOf(cant_preguntas) + "' hidden>"
+                                        + "     <input type='text' id='answer' name='answer' value='X' hidden>"
+                                        + "     <input type='text' id='direccion' name='direccion' value='X' hidden>"
+                                        + "     <input type=\"text\" id=\"Resp\" value=\"" + respuesta + "\" hidden/>"
+                                        + "                </div>\n"
+                                        + "                \n"
+                                        + "                <br />\n"
+                                        + "                <br />\n"
+                                        + "                \n"
+                                        + "                 <p>\n"
+                                        + "                    <label class=\"checkeable\">\n"
+                                        + "                        <input type=\"checkbox\" name=\"Respuesta\" value='1'/>\n"
+                                        + "                        <img src=\"" + ((Element) lista.get(0)).getTextTrim() + "\" />\n"
+                                        + "                    </label>\n"
+                                        + "\n"
+                                        + "                    <label class=\"checkeable\">\n"
+                                        + "                      <input type=\"checkbox\" name=\"Respuesta\" value='2'/>\n"
+                                        + "                      <img src=\"" + ((Element) lista.get(1)).getTextTrim() + "\" />\n"
+                                        + "                    </label>\n"
+                                        + "\n"
+                                        + "                    <label class=\"checkeable\">\n"
+                                        + "                        <input type=\"checkbox\" name=\"Respuesta\" value='3'/>\n"
+                                        + "                        <img src=\"" + ((Element) lista.get(2)).getTextTrim() + "\" />\n"
+                                        + "                    </label>\n"
+                                        + "\n"
+                                        + "                    <label class=\"checkeable\">\n"
+                                        + "                      <input type=\"checkbox\" name=\"Respuesta\" value='4'/>\n"
+                                        + "                      <img src=\"" + ((Element) lista.get(3)).getTextTrim() + "\" />\n"
+                                        + "                    </label>\n"
+                                        + "                </p>\n"
+                                        + "                \n"
+                                        + "                <p>\n"
+                                        + "                    <label class=\"checkeable\">\n"
+                                        + "                        <input type=\"checkbox\" name=\"Respuesta\" value='5'/>\n"
+                                        + "                        <img src=\"" + ((Element) lista.get(4)).getTextTrim() + "\" />\n"
+                                        + "                    </label>\n"
+                                        + "\n"
+                                        + "                    <label class=\"checkeable\">\n"
+                                        + "                      <input type=\"checkbox\" name=\"Respuesta\" value='6'/>\n"
+                                        + "                      <img src=\"" + ((Element) lista.get(5)).getTextTrim() + "\" />\n"
+                                        + "                    </label>\n"
+                                        + "                    <label class=\"checkeable\">\n"
+                                        + "                        <input type=\"checkbox\" name=\"Respuesta\" value='7'/>\n"
+                                        + "                        <img src=\"" + ((Element) lista.get(6)).getTextTrim() + "\" />\n"
+                                        + "                    </label>\n"
+                                        + "\n"
+                                        + "                    <label class=\"checkeable\">\n"
+                                        + "                      <input type=\"checkbox\" name=\"Respuesta\" value='8'/>\n"
+                                        + "                      <img src=\"" + ((Element) lista.get(7)).getTextTrim() + "\" />\n"
+                                        + "                    </label>\n"
+                                        + "                    \n"
+                                        + "                    <img class=\"Ad6\" src=\"" + multimedia + "\" alt=\"Examen\" />\n"
+                                        + "                </p>\n"
+                                        + "                \n"
+                                        + "                \n"
+                                        + "                \n"
+                                        + "                \n"
+                                        + "                 <div id=\"Bottom\" style=\"position: relative; left: 0px; top:20px; background-color:#f0b60f\" >\n"
+                                        + "                     <br />\n"
+                                        + "                     \n"
+                                        + "                     <input type=\"button\" class=\"button2\" onclick='Siguiente()' value=\"Siguiente\" />"
+                                        + "<input type=\"button\" class=\"button2\" onclick=\"Calificar()\" value=\"Calificar\" />\n"
+                                        + "               </div>\n"
+                                        + "            </div>\n"
+                                        + "            \n"
+                                        + "            <div id=\"Left\" style=\"position: relative; left: 100px; top: -100px; border: 4px solid #f0b60f;\">\n"
+                                        + "                <p>Lea cuidadosamente la pregunta\n"
+                                        + "                y seleccione si es verdadera o falsa</p>\n"
+                                        + "                <p>Seleccione <b>Calificar</b> para obtener puntaje</p>\n"
+                                        + "                <p>Seleccione <b>Siguiente</b> para pasar a la otra pregunta</p>\n"
+                                        + "            </div>\n"
+                                        + "            \n"
+                                        + "            <div id=\"Left\" style=\"position: relative; left: -205px; top:130px; border: 4px solid #f0b60f;\">\n"
+                                        + "                <p><b>FeedBack</b></p>\n"
+                                        + "                <p><textarea id='feed' readonly style='width: 290px; height: 120px; background-color: white; text­align: center; border:none;'>" + inicial + "</textarea></p>\n"
+                                        + "            </div>\n"
+                                        + "        </div>");
+
+                                /*
                                 out.println("<style>\n"
                                         + "            \n"
                                         + "            .checkeable input {\n"
@@ -350,7 +442,7 @@ public class ResolverExamen extends HttpServlet {
                                         + "                <p>Seleccione <b>Calificar</b> para obtener puntaje</p>\n"
                                         + "                <p>Seleccione <b>Siguiente</b> para pasar a la otra pregunta</p>\n"
                                         + "            </div>\n"
-                                        + "        </div>");
+                                        + "        </div>");*/
                             }
                             break;
                         }
